4a6941106a726ce233be38de0bfb72803b3c004c
liubin <103818443@qq.com>
2020-08-17 10:29:59 +0800

    上传代码

---
+++ src/main/java/com/zpc/order/service/ItemService.java
@@ -0,0 +1,29 @@
+package com.zpc.order.service;
+
+
+import com.zpc.order.entity.Item;
+import com.zpc.order.properties.OrderProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ItemService {
+
+    // Spring框架对RESTful方式的http请求做了封装，来简化操作
+    @Autowired
+    private RestTemplate restTemplate;
+
+    //@Value("${myspcloud.item.url}")
+    //private String itemUrl;
+
+    @Autowired
+    OrderProperties orderProperties;
+
+    public Item queryItemById(Long id) {
+        return this.restTemplate.getForObject(orderProperties.getItem().getUrl()
+                + id, Item.class);
+    }
+}
+
